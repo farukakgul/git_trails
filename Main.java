e6b02efbfedfbbd88da92a3dc653a6362dd0f53e
farukakgul <omerfarukakgul1999@gmail.com>
2020-10-24 11:33:03 +0300

    fast forward tan sonraki commit

--- Main.java
+++ Main.java
@@ -9,5 +9,6 @@ public class Main {
     System.out.println("smart git i deniyorum ");
     System.out.println("smart git 2. commit ");
     System.out.println("Tekrardan master a geçtim smart gitten deniyorum");
+    System.out.println("fast forward sonrasında tekrar master a yazıyorum");
   }
 }

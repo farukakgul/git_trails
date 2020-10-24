af4a44475e9a08e6c84a341ad1567c8b70982d49
farukakgul <omerfarukakgul1999@gmail.com>
2020-10-24 11:16:28 +0300

    sabah denediğim commit

--- Main.java
+++ Main.java
@@ -8,5 +8,6 @@ public class Main {
     System.out.println("tekrar branchten yazıyorum");
     System.out.println("smart git i deniyorum ");
     System.out.println("smart git 2. commit ");
+    System.out.println("Tekrardan master a geçtim smart gitten deniyorum");
   }
 }
